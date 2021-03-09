1295ce89e0b041295caf80218a35f4c6b761817b
krendil766 <krendilek766@gmail.com>
2021-03-09 10:55:42 +0300

    feat: add fn check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrBreakets = bracketsConfig.map((item) => item.join(""));
+
+  for (let i = 0; i < arrBreakets.length; i++) {
+      if (str.indexOf(arrBreakets[i]) != -1) {
+          console.log(arrBreakets[i]);
+          
+          str = str.replace(arrBreakets[i], '');
+          i = -1;
+      }
+  }
+  return str.length === 0 ? true : false
 }
